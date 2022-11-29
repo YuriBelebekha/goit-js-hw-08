77af46f71b61427f18c6ad1db785e236c68251b3
Yuri Belebekha <ybelebekha@gmail.com>
2022-11-29 20:26:45 +0200

    Finished Task 1.

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+
+const galleryMarkup = createGallery(galleryItems);
+galleryRef.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGallery(galleryItems) {
+  return galleryItems
+  .map(( {preview, original, description} ) => {
+    return `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}"/>
+      </a>
+    </li>
+    `
+  })
+  .join('');  
+};
+
+const lightbox = new SimpleLightbox('.gallery a',
+  {
+    captionsData: 'alt',
+    captionPosition: 'outside',
+    captionDelay: 250,
+  });
