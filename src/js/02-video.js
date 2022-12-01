18c2a65474f410acda014dd3a3dae800b3afa714
Yuri Belebekha <ybelebekha@gmail.com>
2022-12-01 22:52:01 +0200

    Finished Task 2 - Videoplayer.

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+import throttle from "lodash.throttle";
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+const onPauses = function (timeupdate) {  
+  localStorage.setItem("videoplayer-current-time", timeupdate.seconds);
+};
+player.on('pause', throttle(onPauses, 1000));
+
+const savedCurrentTime = localStorage.getItem("videoplayer-current-time");
+player.setCurrentTime(savedCurrentTime).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
