64497755389483c22d0491428392171cb3928760
Yuri Belebekha <ybelebekha@gmail.com>
2022-12-01 23:03:11 +0200

    Fixed some in Task 2 - Videoplayer.

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,10 +3,10 @@ import throttle from "lodash.throttle";
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
 
-const onPauses = function (timeupdate) {  
+const onPlay = function (timeupdate) {  
   localStorage.setItem("videoplayer-current-time", timeupdate.seconds);
 };
-player.on('pause', throttle(onPauses, 1000));
+player.on('timeupdate', throttle(onPlay, 1000));
 
 const savedCurrentTime = localStorage.getItem("videoplayer-current-time");
 player.setCurrentTime(savedCurrentTime).then(function(seconds) {
