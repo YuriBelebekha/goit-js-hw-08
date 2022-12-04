00a143036fb75be6018a927650d6aff934563588
Yuri Belebekha <ybelebekha@gmail.com>
2022-12-04 19:59:57 +0200

    Fixed Task 3 - Feedback form

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,12 +1,9 @@
 import throttle from "lodash.throttle";
 
 const refs = {
-  form: document.querySelector('.feedback-form'), 
-  email: document.querySelector('.feedback-form input'),
-  message: document.querySelector('.feedback-form textarea'),
+  form: document.querySelector('.feedback-form'),  
 }
 
-const feedbackFormData = {};
 const STORAGE_KEY = 'feedback-form-state';
 
 refs.form.addEventListener('submit', onFormSubmit);
@@ -22,16 +19,27 @@ function onFormSubmit(e) {
 };
 
 function onFormInput(e) {  
+  let feedbackFormData = localStorage.getItem(STORAGE_KEY);
+  
+  feedbackFormData = feedbackFormData ? JSON.parse(feedbackFormData) : {};  
   feedbackFormData[e.target.name] = e.target.value;
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(feedbackFormData));
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(feedbackFormData));  
 };
 
 function getDataFromLocalStorage() {
-  const localStorageData = JSON.parse(localStorage.getItem(STORAGE_KEY));  
+  let localStorageData = localStorage.getItem(STORAGE_KEY);  
 
   if (localStorageData) {
-    refs.email.value = localStorageData.email;
-    refs.message.value = localStorageData.message;
-  }  
+    localStorageData = JSON.parse(localStorageData);
+    
+    for (const [key, value] of Object.entries(localStorageData)) {      
+      refs.form.elements[key].value = value;      
+    }    
+  }
 };
 getDataFromLocalStorage();
+
+
+
+
